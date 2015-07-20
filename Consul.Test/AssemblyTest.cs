eb585882f0ba3f647b50b433b8107d0f63826c25
Tiru Srikantha <tiru@playfab.com>
2015-07-20 10:27:24 -0700

    Add strong name signing for Consul assembly
    Fixes #10 by adding a strong name signing key.
    
    The new strong name is:
    
    Consul, Version=0.5.2.8, Culture=neutral, PublicKeyToken=20a6ad9a81df1d95

---
+++ Consul.Test/AssemblyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Consul.Test
+{
+    /// <summary>
+    /// Summary description for AssemblyTest
+    /// </summary>
+    [TestClass]
+    public class AssemblyTest
+    {
+        [TestMethod]
+        public void Assembly_IsStrongNamed()
+        {
+            Type t = typeof(Consul.Client);
+            string s = t.Assembly.FullName.ToString();
+            Trace.WriteLine(s);
+            Assert.IsTrue(t.Assembly.FullName.Contains("PublicKeyToken"));
+        }
+    }
+}
