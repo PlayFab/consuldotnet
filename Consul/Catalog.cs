8cc0def49ab0c57dad061efebc10e2b9367925e3
Highly Unavailable <highlyunavailable@users.noreply.github.com>
2015-10-29 21:04:03 -0700

    Add comment explaining why node is named name

--- Consul/Catalog.cs
+++ Consul/Catalog.cs
@@ -24,6 +24,8 @@ namespace Consul
 {
     public class Node
     {
+        // Cannot be "Node" as in the Go API because in C#, properties cannot
+        // have the same name as their enclosing class.
         [JsonProperty(PropertyName = "Node")]
         public string Name { get; set; }
         public string Address { get; set; }
