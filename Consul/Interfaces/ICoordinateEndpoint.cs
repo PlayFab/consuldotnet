336faaee29b1da2a69bcb86bce64a075c1a2779a
Highly Unavailable <highlyunavailable@gmail.com>
2015-11-08 23:34:30 -0700

    Extract interface for coordinate endpoint

--- Consul/Interfaces/ICoordinateEndpoint.cs
+++ Consul/Interfaces/ICoordinateEndpoint.cs
@@ -2,5 +2,8 @@
 {
     public interface ICoordinateEndpoint
     {
+        QueryResult<CoordinateDatacenterMap[]> Datacenters();
+        QueryResult<CoordinateEntry[]> Node();
+        QueryResult<CoordinateEntry[]> Node(QueryOptions q);
     }
 }
