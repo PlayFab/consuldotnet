ccebafbe7462a168c6bf4160e2f744e98ec224a0
Highly Unavailable <highlyunavailable@gmail.com>
2016-02-11 22:25:00 -0700

    Remove finalizer

--- Consul/Client.cs
+++ Consul/Client.cs
@@ -437,11 +437,11 @@ namespace Consul
             }
         }
 
-        ~ConsulClient()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(false);
-        }
+        //~ConsulClient()
+        //{
+        //    // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //    Dispose(false);
+        //}
 
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
