d8a4ffe29f3c164db40a47d93d8b1db8e0e17fb2
Highly Unavailable <highlyunavailable@gmail.com>
2016-07-05 00:57:40 -0700

    Add signing again

--- Consul/Properties/AssemblyInfo.cs
+++ Consul/Properties/AssemblyInfo.cs
@@ -35,7 +35,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.6.1.0")]
 [assembly: AssemblyFileVersion("0.6.1.0")]
-[assembly: InternalsVisibleTo("Consul.Test")]
 [assembly: InternalsVisibleTo("Consul.Test, PublicKey=" +
     "002400000480000094000000060200000024000052534131000400000100010045f6337bf03a95" +
     "218f1e0b4e70bb91f8ee49fcb58593d78c1008ee646cfcf785ea60bd0b1dde6f5f92ead738e2bd" +
